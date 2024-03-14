1f52050af434c0745be738b2048852d67951387a
Dinesh726544 <bobi726544@gmail.com>
2024-03-14 19:53:53 +0530

    with the help of hooks i have update all the changes

--- PasswordGnerator/src/App.jsx
+++ PasswordGnerator/src/App.jsx
@@ -3,13 +3,17 @@ import { useState, useCallback, useEffect, useRef } from 'react'
 
 
 function App() {
-
   const [length, setLength] = useState(8)
   const [numberAllowed, setNumberAllowed] = useState(false);
   const [charAllowed, setCharAllowed] = useState(false)
   const [password, setPassword] = useState("")
+  const [buttonClicked, setButtonClicked] = useState(false); // New state for tracking button click
 
-  const passwordGenerator = () => {
+
+  //useRef hook
+  const passwordRef = useRef(null)
+
+  const passwordGenerator = useCallback(() => {
     let pass = ""
     let str = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz"
     if (numberAllowed) str += "0123456789"
@@ -23,10 +27,20 @@ function App() {
 
     setPassword(pass)
 
-  }
 
-  
+  }, [length, numberAllowed, charAllowed, setPassword])
 
+  const copyPasswordToClipboard = useCallback(() => {
+    passwordRef.current?.select();
+    passwordRef.current?.setSelectionRange(0, 999);
+    setButtonClicked(true); // Set button clicked to true when clicked
+    window.navigator.clipboard.writeText(password)
+  }, [password])
+
+  useEffect(() => {
+    passwordGenerator()
+    setButtonClicked(false); // Reset button clicked state whenever password changes
+  }, [length, numberAllowed, charAllowed, passwordGenerator])
   return (
     
     <div className="w-full max-w-md mx-auto shadow-md rounded-lg px-4 py-3 my-8 bg-gray-800 text-orange-500">
@@ -38,10 +52,14 @@ function App() {
             className="outline-none w-full py-1 px-3"
             placeholder="Password"
             readOnly
+            ref={passwordRef}
         />
         <button
-        className='outline-none bg-blue-700 text-white px-3 py-0.5 shrink-0'
-        >copy</button>
+          onClick={copyPasswordToClipboard}
+          className={`outline-none px-3 py-0.5 ${buttonClicked ? 'bg-green-500' : 'bg-blue-700'} text-white`}
+        >
+          {buttonClicked ? 'Copied' : 'Copy'}
+        </button>
         
     </div>
     <div className='flex text-sm gap-x-2'>
@@ -79,8 +97,6 @@ function App() {
           <label htmlFor="characterInput">Characters</label>
       </div>
     </div>
-    <button onClick={passwordGenerator}>oo</button>
-    {/* {passwordGenerator()} */}
 </div>
     
   )
