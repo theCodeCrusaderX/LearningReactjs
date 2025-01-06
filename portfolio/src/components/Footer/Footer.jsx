0e5b637d7b4a5fbd3fa15360f85b0ef5d45f98e8
theCodeCrusaderX <bobi726544@gmail.com>
2025-01-06 17:45:14 +0530

    updated resume

--- portfolio/src/components/Footer/Footer.jsx
+++ portfolio/src/components/Footer/Footer.jsx
@@ -29,7 +29,7 @@ export default function Footer() {
             
 
             <Link
-              to="https://x.com/Code__Crusader"
+              to="https://x.com/theCodeCrusader"
               className={`text-gray-500 ${
                 theme ? "hover:text-white" : "hover:text-gray-900"
               }`}
